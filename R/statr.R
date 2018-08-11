6744a1d1754112d545cf8dd47896bb029ced5357
JA-Win <jay.achar@doctors.org.uk>
2018-08-11 07:53:21 +0200

    init commit for normal_checkr()

---
+++ R/statr.R
@@ -0,0 +1,11 @@
+#' statr: A package of personal statistics tools.
+#'
+#' The package provides functions to analyse and present data using statistical methods
+#' 
+#' 
+#' @author Jay Achar \email{jay.achar@@doctors.org.uk}
+#' @seealso \code{\link{normal_checkr}} 
+#' 
+#' @docType package
+#' @name statr
+NULL
